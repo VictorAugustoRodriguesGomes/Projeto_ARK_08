2982e9602376c6dcd75ad0776552232ad93811e0
Victor Augusto <victor08022000@gmail.com>
2022-06-08 00:03:31 -0300

    Imagem Base

--- ARK-08/src/main/java/ark08/ARK08.java
+++ ARK-08/src/main/java/ark08/ARK08.java
@@ -5,31 +5,45 @@ import javax.swing.JOptionPane;
 public class ARK08 {
 
     public static void main(String[] args) {
+        int numero1 = 0;
+        int numero2 = 0;
         int resutado;
-        int numero1;
-        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
-        int numero2;
-        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+
+        try {
+            numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Valor Inválido");
+            System.exit(0);
+        }
+
+        try {
+            numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Valor Inválido");
+            System.exit(0);
+        }
+
         String opcao = JOptionPane.showInputDialog("Informe um sinal de operação(+, -, * ou /) :");
+
         switch (opcao) {
             case "+":
                 resutado = numero1 + numero2;
-                JOptionPane.showMessageDialog(null, "O resutado é :" + resutado);
+                JOptionPane.showMessageDialog(null, "O resutado é : " + resutado);
                 break;
             case "-":
                 resutado = numero1 - numero2;
-                JOptionPane.showMessageDialog(null, "O resutado é :" + resutado);
+                JOptionPane.showMessageDialog(null, "O resutado é : " + resutado);
                 break;
             case "*":
                 resutado = numero1 * numero2;
-                JOptionPane.showMessageDialog(null, "O resutado é :" + resutado);
+                JOptionPane.showMessageDialog(null, "O resutado é : " + resutado);
                 break;
             case "/":
                 resutado = numero1 / numero2;
-                JOptionPane.showMessageDialog(null, "O resutado é :" + resutado);
+                JOptionPane.showMessageDialog(null, "O resutado é : " + resutado);
                 break;
             default:
-                JOptionPane.showMessageDialog(null, "valor invalido");
+                JOptionPane.showMessageDialog(null, "Valor Inválido");
                 break;
         }
     }
